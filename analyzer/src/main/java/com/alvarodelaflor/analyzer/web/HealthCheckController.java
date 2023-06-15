13da5e5308abc9d5198b405da1370d0c83203fde
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2023-06-15 20:04:53 +0200

    New postman

---
+++ analyzer/src/main/java/com/alvarodelaflor/analyzer/web/HealthCheckController.java
@@ -0,0 +1,15 @@
+package com.alvarodelaflor.analyzer.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/healthcheck")
+public class HealthCheckController {
+
+    @GetMapping("/ping")
+    public String ping() {
+        return "pong";
+    }
+}
