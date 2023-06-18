079ea10d40b47658f8ccfd9bee7d0ae9e5c9c086
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2023-06-18 20:34:45 +0200

    Add execution module

---
+++ execution/src/test/java/com/alvarodelaflor/execution/ExecutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvarodelaflor.execution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
