85a5bc9a6eda73a8c2bb2e237a0cefbcaf426021
Alvaro de la Flor Bonilla <alvarodelaflor@gmail.com>
2023-06-26 10:26:26 +0200

    Logger

--- execution/src/main/java/com/alvarodelaflor/execution/services/ActionService.java
+++ execution/src/main/java/com/alvarodelaflor/execution/services/ActionService.java
@@ -2,17 +2,21 @@ package com.alvarodelaflor.execution.services;
 
 import com.alvarodelaflor.domain.model.Workbook;
 import com.alvarodelaflor.domain.model.alerts.AlertType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ActionService {
+
+    private static final Logger logger = LoggerFactory.getLogger(ActionService.class);
 
     public void executeAction(Workbook workbook) {
         workbook.getCommonAlerts().stream()
                 .filter(alert -> alert.getAlertType().contains(AlertType.ACTION))
                 .forEach(alert-> {
                     // TODO
-                    System.out.println("Execute: " + alert.getSummary());
+                    logger.info(String.format("EXECUTE ACTION: %s", alert.getSummary()));
                 });
     }
 }
